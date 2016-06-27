44b17ffbb271d5fa5edad1107f37576525a3ee8d
marcnegri <marc.negri@nh-partners.com>
2016-06-27 11:05:57 +0200

    [Marc] - 27.06.2016 + Add Events Class + Add MyNextEventsView

---
+++ UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/Business/Events.cs
@@ -0,0 +1,96 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouWeesh.Mobile.Business
+{
+    public class Events
+    {
+        #region PROPERTIES
+        private String title;
+        private String startDatetime;
+        private String endDatetime;
+        private List<Users> participants;
+        private String location;
+        private String picture;
+        #endregion
+
+        #region ACCESSORS
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+        public string StartDatetime
+        {
+            get
+            {
+                return startDatetime;
+            }
+
+            set
+            {
+                startDatetime = value;
+            }
+        }
+        public string EndDatetime
+        {
+            get
+            {
+                return endDatetime;
+            }
+
+            set
+            {
+                endDatetime = value;
+            }
+        }
+        public List<Users> Participants
+        {
+            get
+            {
+                return participants;
+            }
+
+            set
+            {
+                participants = value;
+            }
+        }
+        public string Location
+        {
+            get
+            {
+                return location;
+            }
+
+            set
+            {
+                location = value;
+            }
+        }
+        public string Picture
+        {
+            get
+            {
+                return picture;
+            }
+
+            set
+            {
+                picture = value;
+            }
+        }
+
+        #endregion
+    }
+}
