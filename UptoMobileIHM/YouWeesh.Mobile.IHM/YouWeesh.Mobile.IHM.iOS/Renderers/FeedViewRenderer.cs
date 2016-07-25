724c4b07d602bc59cc839e2cdc0bcb9d6293ceed
vincehar <haring.vincent@gmail.com>
2016-07-25 11:55:36 +0200

    - FeedView

---
+++ UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM.iOS/Renderers/FeedViewRenderer.cs
@@ -0,0 +1,54 @@
+﻿using System;
+using YouWeesh.Mobile.Views;
+using Xamarin.Forms;
+using YouWeesh.Mobile.IHM.iOS;
+using Xamarin.Forms.Platform.iOS;
+using UIKit;
+using System.Collections.Generic;
+
+[assembly:ExportRenderer(typeof(FeedView), typeof(FeedViewRenderer))]
+namespace YouWeesh.Mobile.IHM.iOS
+{
+	public class FeedViewRenderer : PageRenderer
+	{
+		public new FeedView Element
+		{
+			get { return (FeedView)base.Element; }
+		}
+
+		public override void ViewWillAppear(bool animated)
+		{
+			base.ViewWillAppear(animated);
+
+			var LeftNavList = new List<UIBarButtonItem>();
+			var rightNavList = new List<UIBarButtonItem>();
+
+			var navigationItem = this.NavigationController.TopViewController.NavigationItem;
+
+			UIBarButtonItem menu = navigationItem.LeftBarButtonItem;
+			LeftNavList.Add(menu);
+			for (var i = 0; i < Element.ToolbarItems.Count; i++)
+			{
+
+				var reorder = (Element.ToolbarItems.Count - 1);
+				var ItemPriority = Element.ToolbarItems[reorder - i].Priority;
+
+				if (ItemPriority == 1)
+				{
+					UIBarButtonItem LeftNavItems = navigationItem.RightBarButtonItems[i];
+					LeftNavList.Add(LeftNavItems);
+				}
+				else if (ItemPriority == 0)
+				{
+					UIBarButtonItem RightNavItems = navigationItem.RightBarButtonItems[i];
+					rightNavList.Add(RightNavItems);
+				}
+			}
+
+			navigationItem.SetLeftBarButtonItems(LeftNavList.ToArray(), false);
+			navigationItem.SetRightBarButtonItems(rightNavList.ToArray(), false);
+
+		}
+	}
+}
+
--- UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/Views/FeedView.xaml.cs
+++ UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/Views/FeedView.xaml.cs
@@ -9,111 +9,136 @@ using Xamarin.Forms.Maps;
 
 namespace YouWeesh.Mobile.Views
 {
-    public partial class FeedView : TabbedPage
-    {
-        ObservableCollection<Weesh> weeshes = new ObservableCollection<Weesh>();
-        bool filterVisible = false;
-        FeedFilterForm feedFilterForm = new FeedFilterForm();
+	public partial class FeedView : ContentPage
+	{
+		ObservableCollection<Weesh> weeshes = new ObservableCollection<Weesh>();
+		bool filterVisible = false;
+		FeedFilterForm feedFilterForm = new FeedFilterForm();
 
-        public FeedView()
-        {
-            InitializeComponent();
+		public FeedView()
+		{
+			InitializeComponent();
 
-            weeshes.Add(new Weesh { Title = "Jouer au #foot", Creator = "Vince", Image = "portrait.jpg", CreationDate = "01h30 ago", Distance = "1 km" });
-            weeshes.Add(new Weesh { Title = "Faire une partie de #tennis", Creator = "Marcus", Image = "portrait.jpg", CreationDate = "03h30 ago", Distance = "1,5 km" });
-            weeshes.Add(new Weesh { Title = "Motivé pour aller au #theatre", Creator = "James", Image = "portrait.jpg", CreationDate = "06h00 ago", Distance = "0.5 km" });
-            weeshes.Add(new Weesh { Title = "#courrir !!", Creator = "Nadia", Image = "portrait.jpg", CreationDate = "06h15 ago", Distance = "2 km" });
-            weeshes.Add(new Weesh { Title = "Discuter autour d'un #verre", Creator = "Jenny", Image = "portrait.jpg", CreationDate = "06h45 ago", Distance = "1,2 km" });
-            weeshes.Add(new Weesh { Title = "Aller regarder l'#euro", Creator = "Loic", Image = "portrait.jpg", CreationDate = "07h00 ago", Distance = "2,6 km" });
-            weeshes.Add(new Weesh { Title = "Je vais mater l'#euro a la #fanzone", Creator = "Marcus", Image = "portrait.jpg", CreationDate = "07h15 ago", Distance = "0.6 km" });
-            weeshes.Add(new Weesh { Title = "J'ai faim de  #nectarine", Creator = "Vivi", Image = "portrait.jpg", CreationDate = "12h00 ago", Distance = "0,3 km" });
+			//Add Buttons on top bar
+			ToolbarItems.Add(new ToolbarItem
+			{
+				Text = "List",
+				Icon = "Listviewicon.png",
+				Order = ToolbarItemOrder.Primary,
+				Priority = 0
+			});
 
-            feedView.ItemsSource = weeshes;
+			ToolbarItems.Add(new ToolbarItem
+			{
+				Text = "Map",
+				Icon = "Mappinicon.png",
+				Order = ToolbarItemOrder.Primary,
+				Priority = 0
+			});
 
-            var positionMarcus = new Position(46.1914286, 6.1354516);
-            var pinMarcus = new Pin
-            {
-                Type = PinType.Place,
-                Position = positionMarcus,
-                Label = "Marcus",
-                Address = "Marcus Home"
-            };
-            MyMap.Pins.Add(pinMarcus);          
+			ToolbarItems.Add(new ToolbarItem
+			{
+				Text = "Filter",
+				Icon = "Filtericon.png",
+				Order = ToolbarItemOrder.Primary,
+				Priority = 1
+			});
 
-            // 1 - test hide /unhide during an event (scroll)
-            // grdListType.HeightRequest = 0;
+			weeshes.Add(new Weesh { Title = "Jouer au #foot", Creator = "Vince", Image = "portrait.jpg", CreationDate = "01h30 ago", Distance = "1 km" });
+			weeshes.Add(new Weesh { Title = "Faire une partie de #tennis", Creator = "Marcus", Image = "portrait.jpg", CreationDate = "03h30 ago", Distance = "1,5 km" });
+			weeshes.Add(new Weesh { Title = "Motivé pour aller au #theatre", Creator = "James", Image = "portrait.jpg", CreationDate = "06h00 ago", Distance = "0.5 km" });
+			weeshes.Add(new Weesh { Title = "#courrir !!", Creator = "Nadia", Image = "portrait.jpg", CreationDate = "06h15 ago", Distance = "2 km" });
+			weeshes.Add(new Weesh { Title = "Discuter autour d'un #verre", Creator = "Jenny", Image = "portrait.jpg", CreationDate = "06h45 ago", Distance = "1,2 km" });
+			weeshes.Add(new Weesh { Title = "Aller regarder l'#euro", Creator = "Loic", Image = "portrait.jpg", CreationDate = "07h00 ago", Distance = "2,6 km" });
+			weeshes.Add(new Weesh { Title = "Je vais mater l'#euro a la #fanzone", Creator = "Marcus", Image = "portrait.jpg", CreationDate = "07h15 ago", Distance = "0.6 km" });
+			weeshes.Add(new Weesh { Title = "J'ai faim de  #nectarine", Creator = "Vivi", Image = "portrait.jpg", CreationDate = "12h00 ago", Distance = "0,3 km" });
 
-        }
+			feedView.ItemsSource = weeshes;
 
-        private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
-        {
-            feedView.BeginRefresh();
+			var positionMarcus = new Position(46.1914286, 6.1354516);
+			var pinMarcus = new Pin
+			{
+				Type = PinType.Place,
+				Position = positionMarcus,
+				Label = "Marcus",
+				Address = "Marcus Home"
+			};
+			//MyMap.Pins.Add(pinMarcus);          
 
-            if (string.IsNullOrWhiteSpace(e.NewTextValue))
-                feedView.ItemsSource = weeshes;
-            else
-                feedView.ItemsSource = weeshes.Where(i => i.Title.ToLower().Contains(e.NewTextValue) || i.Creator.ToLower().Contains(e.NewTextValue));
+			// 1 - test hide /unhide during an event (scroll)
+			// grdListType.HeightRequest = 0;
 
-            feedView.EndRefresh();
-        }
+		}
 
-        private void ImageCell_OnTapped(object sender, EventArgs e)
-        {
-            //grdListType.HeightRequest = 60;
-        }
+		private void SearchBar_OnTextChanged(object sender, TextChangedEventArgs e)
+		{
+			feedView.BeginRefresh();
 
-        /// <summary>
-        /// 
-        /// </summary>
-        /// <param name="sender"></param>
-        /// <param name="e"></param>
-        private void feedView_OnItemAppaering(object sender, EventArgs e)
-        {
-            //grdListType.HeightRequest = 60;
-        }
+			if (string.IsNullOrWhiteSpace(e.NewTextValue))
+				feedView.ItemsSource = weeshes;
+			else
+				feedView.ItemsSource = weeshes.Where(i => i.Title.ToLower().Contains(e.NewTextValue) || i.Creator.ToLower().Contains(e.NewTextValue));
 
-        private void scrlMainView_Scrolled(object sender, ScrolledEventArgs e)
-        {
+			feedView.EndRefresh();
+		}
 
-            if (((ScrollView)sender).ScrollY < e.ScrollY)
-            {
-                feedView.BackgroundColor = Color.Aqua;
-            }
-        }
+		private void ImageCell_OnTapped(object sender, EventArgs e)
+		{
+			//grdListType.HeightRequest = 60;
+		}
 
-        async void BtnFilter_Clicked(object sender, EventArgs e)
-        {
-            //var action = await DisplayActionSheet("ActionSheet: Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
-            //await Navigation.PushModalAsync(new FeedFilterForm());           
+		/// <summary>
+		/// 
+		/// </summary>
+		/// <param name="sender"></param>
+		/// <param name="e"></param>
+		private void feedView_OnItemAppaering(object sender, EventArgs e)
+		{
+			//grdListType.HeightRequest = 60;
+		}
 
-            filterVisible = !filterVisible;
+		private void scrlMainView_Scrolled(object sender, ScrolledEventArgs e)
+		{
 
-            if (filterVisible == true)
-            {
-                relativeLayout.Children.Add(feedFilterForm, Constraint.Constant(0),Constraint.Constant(-400));
-                await feedFilterForm.LayoutTo(new Rectangle(0, 0, feedFilterForm.Width, feedFilterForm.Height), 250);                
-            }
-            else
-            {
-                await feedFilterForm.LayoutTo(new Rectangle(0, -1 * feedFilterForm.Height, feedFilterForm.Width, feedFilterForm.Height), 250);
-                relativeLayout.Children.Remove(feedFilterForm);
-            }
+			if (((ScrollView)sender).ScrollY < e.ScrollY)
+			{
+				feedView.BackgroundColor = Color.Aqua;
+			}
+		}
+
+		async void BtnFilter_Clicked(object sender, EventArgs e)
+		{
+			//var action = await DisplayActionSheet("ActionSheet: Send to?", "Cancel", null, "Email", "Twitter", "Facebook");
+			//await Navigation.PushModalAsync(new FeedFilterForm());           
+
+			filterVisible = !filterVisible;
+
+			if (filterVisible == true)
+			{
+				relativeLayout.Children.Add(feedFilterForm, Constraint.Constant(0), Constraint.Constant(-400));
+				await feedFilterForm.LayoutTo(new Rectangle(0, 0, feedFilterForm.Width, feedFilterForm.Height), 250);
+			}
+			else
+			{
+				await feedFilterForm.LayoutTo(new Rectangle(0, -1 * feedFilterForm.Height, feedFilterForm.Width, feedFilterForm.Height), 250);
+				relativeLayout.Children.Remove(feedFilterForm);
+			}
 
 
-        }
+		}
 
 
 
-    }
+	}
 
-    public class Weesh
-    {
-        public string Title { get; set; }
-        public string Creator { get; set; }
-        public string Image { get; set; }
-        public string CreationDate { get; set; }
-        public string Distance { get; set; }
-    }
+	public class Weesh
+	{
+		public string Title { get; set; }
+		public string Creator { get; set; }
+		public string Image { get; set; }
+		public string CreationDate { get; set; }
+		public string Distance { get; set; }
+	}
 
 
 }
