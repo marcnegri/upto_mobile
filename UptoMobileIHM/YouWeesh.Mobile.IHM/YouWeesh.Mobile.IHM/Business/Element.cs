674b078f89d47642fb6ba1cc39435497f7aec696
vincehar <haring.vincent@gmail.com>
2016-07-25 18:39:16 +0200

    gtrrg

---
+++ UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/Business/Element.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace YouWeesh.Mobile.Business
+{
+    public class Element
+    {
+        #region PROPERTIES
+        private String title;
+        private String startDatetime;
+        private String endDatetime;
+        private String description;
+        private List<Users> participants;
+        private Users creator;
+		private String creationDate;
+        private String location;
+        private String picture;
+		private Boolean isEvent;
+        #endregion
+
+        #region ACCESSORS
+        public string Title
+        {
+            get
+            {
+                return title;
+            }
+
+            set
+            {
+                title = value;
+            }
+        }
+        public string StartDatetime
+        {
+            get
+            {
+                return startDatetime;
+            }
+
+            set
+            {
+                startDatetime = value;
+            }
+        }
+        public string EndDatetime
+        {
+            get
+            {
+                return endDatetime;
+            }
+
+            set
+            {
+                endDatetime = value;
+            }
+        }
+        public List<Users> Participants
+        {
+            get
+            {
+                return participants;
+            }
+
+            set
+            {
+                participants = value;
+            }
+        }
+        public string Location
+        {
+            get
+            {
+                return location;
+            }
+
+            set
+            {
+                location = value;
+            }
+        }
+        public string Picture
+        {
+            get
+            {
+                return picture;
+            }
+
+            set
+            {
+                picture = value;
+            }
+        }
+        public Users Creator
+        {
+            get
+            {
+                return creator;
+            }
+
+            set
+            {
+                creator = value;
+            }
+        }
+
+        public string Description
+        {
+            get
+            {
+                return description;
+            }
+
+            set
+            {
+                description = value;
+            }
+        }
+
+		public string CreationDate
+		{
+			get
+			{
+				return creationDate;
+			}
+
+			set
+			{
+				creationDate = value;
+
+			}
+		}
+
+		public Boolean IsEvent
+		{
+			get
+			{
+				return isEvent;
+			}
+
+			set
+			{
+				isEvent = value;
+
+			}
+		}
+
+
+		#endregion
+	}
+}
---
+++ UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/CustomComponents/FeedViewTemplateSelector.cs
@@ -0,0 +1,28 @@
+﻿using System;
+using Xamarin.Forms;
+using YouWeesh.Mobile.Business;
+
+namespace YouWeesh.Mobile.CustomComponents
+{
+	class FeedViewTemplateSelector : Xamarin.Forms.DataTemplateSelector
+	{
+		public FeedViewTemplateSelector()
+		{
+			// Retain instances!
+			this.eventDataTemplate = new DataTemplate(typeof(EventsViewCell));
+			this.weeshDataTemplate = new DataTemplate(typeof(WeeshViewCell));
+		}
+
+		protected override DataTemplate OnSelectTemplate(object item, BindableObject container)
+		{
+			var element = item as Business.Element;
+			if (element == null)
+				return null;
+			return element.IsEvent ? this.eventDataTemplate : this.weeshDataTemplate;
+		}
+
+		private readonly DataTemplate eventDataTemplate;
+		private readonly DataTemplate weeshDataTemplate;
+	}
+}
+
