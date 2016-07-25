705b9375a826ef654ce29150e364e09c60d50a70
marcnegri <marc.negri@nh-partners.com>
2016-07-25 11:31:41 +0200

    Add images

--- UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM.iOS/Main.cs
+++ UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM.iOS/Main.cs
@@ -1,8 +1,8 @@
 ﻿using System;
 using System.Collections.Generic;
 using System.Linq;
-
 using Foundation;
+using ImageCircle.Forms.Plugin.iOS;
 using UIKit;
 
 namespace YouWeesh.Mobile.IHM.iOS
@@ -12,9 +12,7 @@ namespace YouWeesh.Mobile.IHM.iOS
         // This is the main entry point of the application.
         static void Main(string[] args)
         {
-            // if you want to use a different Application Delegate class from "AppDelegate"
-            // you can specify it here.
-            UIApplication.Main(args, null, "AppDelegate");
+			UIApplication.Main(args, null, "AppDelegate");
         }
     }
 }
