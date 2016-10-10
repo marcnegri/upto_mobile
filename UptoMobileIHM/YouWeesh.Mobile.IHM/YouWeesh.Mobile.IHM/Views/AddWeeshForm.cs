1cf372be243fb73d5e70db8aeef84e261ec6415c
vincehar <haring.vincent@gmail.com>
2016-10-10 10:44:03 +0200

    - AddWeesh form and Login page

--- UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/Views/AddWeeshForm.cs
+++ UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/Views/AddWeeshForm.cs
@@ -1,21 +1,29 @@
 ﻿using System;
 using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using ImageCircle.Forms.Plugin.Abstractions;
 
 using Xamarin.Forms;
 
 namespace YouWeesh.Mobile.Views
 {
-	public partial class AddWeeshForm : ContentPage
-	{
-		public AddWeeshForm()
-		{
-			InitializeComponent();
-		}
+    public partial class AddWeeshForm : ContentPage
+    {
+        public AddWeeshForm()
+        {
+           // this.BackgroundColor = Color.White;
+            InitializeComponent();
+            //imgWeesh.Aspect = Aspect.AspectFit;
+
+			//normalFab.SizeChanged += (sender, args) => { layout.ForceLayout(); };
+        }
 
 		public void btnAddWeesh_Clicked(object sender, EventArgs e)
 		{
 			this.Navigation.PopModalAsync();
-			MessagingCenter.Send<AddWeeshForm, String>(this, "NewWeesh", editorWeesh.Text);
+			MessagingCenter.Send<AddWeeshForm, String>(this, "NewWeesh", txtWeesh.Text);
 			//this.Navigation.PushModalAsync(new Views.WeeshesTesst());
 		}
 
@@ -25,6 +33,5 @@ namespace YouWeesh.Mobile.Views
 			//MessagingCenter.Send<AddWeeshForm, String>(this, "NewWeesh", editorWeesh.Text);
 			//this.Navigation.PushModalAsync(new Views.WeeshesTesst());
 		}
-	}
+    }
 }
-
