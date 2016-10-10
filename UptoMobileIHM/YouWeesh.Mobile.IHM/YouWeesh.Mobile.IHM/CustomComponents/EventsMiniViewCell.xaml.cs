c8858cf90502d2f91ad6de84c613337286f70e9e
Marc Negri <marc.negri@nh-partners.com>
2016-10-10 19:20:31 +0200

    10.10.2016 + Modify profile page

---
+++ UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/CustomComponents/EventsMiniViewCell.xaml.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using Xamarin.Forms;
+
+namespace YouWeesh.Mobile.CustomComponents
+{
+	public partial class EventsMiniViewCell : ViewCell
+	{
+		public EventsMiniViewCell()
+		{
+			InitializeComponent();
+		}
+	}
+}
--- UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/Views/MyProfileView.xaml.cs
+++ UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/Views/MyProfileView.xaml.cs
@@ -63,13 +63,9 @@ namespace YouWeesh.Mobile
 
 
 
-			/*
+
 			ObservableCollection<Weesh> weeshesList = new ObservableCollection<Weesh>();
 			ObservableCollection<Business.Events> events = new ObservableCollection<Business.Events>();
-
-			Weesh we = new Weesh("Je veux jouer aux #echecs");
-			Weesh we2 = new Weesh("Pourquoi pas aller #courir ce soir ?");
-			Weesh we3 = new Weesh("Je pars #naviguer en #bateau, ca tente quelqu'un ?");
 
 			Events ev1 = new Business.Events { Title = "Trail des Roussets", StartDatetime = "2:30 ago", EndDatetime = "<-> 205km", Picture = "trail.jpg", Location = "Centre sportif du bout du monde", Description = "Samedi une marche découverte et dimanche les courses avec 3 distances : 12 km, 20 km et 32 km" };
 			Events ev2 = new Business.Events { Title = "Tennis playing", StartDatetime = "18:00", EndDatetime = "20:00", Picture = "tennis.jpg", Location = "Centre sportif du bout du monde", Description = "Grand tournoi de Tennis au bout du monde. Un public en folie avec une journée pleine de grands matchs. Venez nombreux !" };
@@ -77,7 +73,7 @@ namespace YouWeesh.Mobile
 
 			events.Add(ev1);
 			events.Add(ev2);
-			events.Add(ev3);*/
+			events.Add(ev3);
 			/*
 			weeshesList.Add(we);
 			weeshesList.Add(we2);
@@ -85,7 +81,7 @@ namespace YouWeesh.Mobile
 
 			lstRecentWeeshes.ItemsSource = elements;
 
-			//lstRecentEvents.ItemsSource = events;
+			lstRecentEvents.ItemsSource = events;
 
 		}
 		/// <summary>
