058cff0f1d2cdda8fea7f0ff519192fbd6416c78
Marc Negri <marc.negri@nh-partners.com>
2016-10-20 20:56:10 +0200

    + Modify coloured icon size on EventDetails + Add orange panel (chat and infos)

--- UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/Views/EventDetailsView.xaml.cs
+++ UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/Views/EventDetailsView.xaml.cs
@@ -3,6 +3,9 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
+using System.Collections.Generic;
+using System.Collections.ObjectModel;
+using YouWeesh.Mobile.Business;
 
 using Xamarin.Forms;
 
@@ -14,7 +17,19 @@ namespace YouWeesh.Mobile.Views
         {
             InitializeComponent();
 			this.BackgroundColor = Color.FromHex("#ececec");
+			Title = "Trail des Roussets";
 
+			/*
+			ObservableCollection<Events> lstEventPictures = new ObservableCollection<Events>();
+
+
+			Events ev1 = new Business.Events { Title = "Trail des Roussets", StartDatetime = "2:30 ago", EndDatetime = "<-> 205km", Picture = "trail.png", Location = "Centre sportif du bout du monde", Description = "Samedi une marche découverte et dimanche les courses avec 3 distances : 12 km, 20 km et 32 km" };
+			Events ev2 = new Business.Events { Title = "Tennis playing", StartDatetime = "18:00", EndDatetime = "20:00", Picture = "trail.png", Location = "Centre sportif du bout du monde", Description = "Grand tournoi de Tennis au bout du monde. Un public en folie avec une journée pleine de grands matchs. Venez nombreux !" };
+			Events ev3 = new Business.Events { Title = "Poker party", StartDatetime = "18:00", EndDatetime = "20:00", Picture = "trail.png", Location = "Pickwick Bar", Description = "Final des WPT avec Patrick Bruel en Guest Star ! Du lourd " };
+
+			//MyDataSource.Add("test1");
+			//MyDataSource.Add("test2");
+			crvEventPictures.ItemsSource = lstEventPictures;*/
 		}
     }
 }
