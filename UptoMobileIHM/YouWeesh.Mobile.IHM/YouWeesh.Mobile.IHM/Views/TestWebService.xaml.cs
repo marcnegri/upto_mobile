55ed0c8d309b12b5e2f838f5677fb3f05852bdfe
vincehar <haring.vincent@gmail.com>
2016-11-28 14:51:37 +0100

    first commit

---
+++ UptoMobileIHM/YouWeesh.Mobile.IHM/YouWeesh.Mobile.IHM/Views/TestWebService.xaml.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+
+using Xamarin.Forms;
+
+namespace YouWeesh.Mobile
+{
+	public partial class TestWebService : ContentPage
+	{
+		public TestWebService()
+		{
+			InitializeComponent();
+		}
+	}
+}
